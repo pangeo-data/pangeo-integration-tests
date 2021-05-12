ea5105f1437cf911984deca01a27cda81264d44d
Noah D. Brenowitz <nbren12@gmail.com>
2021-05-11 18:30:12 -0700

    add test for gcsfs retry logic for writes
    gcsfs 2020.04.0 fails this test

--- test_gcs.py
+++ test_gcs.py
@@ -1,6 +1,8 @@
 import fsspec
 import xarray
 import dask.array
+import asyncio
+import gcsfs
 
 
 def test_write_zarr(tmp_gcs_path):
@@ -14,3 +16,20 @@ def test_write_zarr(tmp_gcs_path):
     )
     mapper = fsspec.get_mapper(tmp_gcs_path)
     ds.to_zarr(mapper)
+
+
+def test_write_quickly_to_object_in_parallel(tmp_gcs_path):
+    """Google has an api limit for writing to an object of 1/sec
+
+    This is a potent test of gcsfs' retry logic for writes
+    """
+    loop = asyncio.get_event_loop()
+    fs = gcsfs.GCSFileSystem(asynchronous=True, loop=None)
+
+    data = b"hello world"
+
+    writes = asyncio.gather(*[fs._pipe_file(tmp_gcs_path, data) for _ in range(40)])
+    loop.run_until_complete(writes)
+
+    fs = gcsfs.GCSFileSystem()
+    assert fs.cat(tmp_gcs_path) == data
